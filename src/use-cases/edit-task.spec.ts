c34aeef8aad6734648045a9b745b05142fef88fb
Guilherme <gui.barros.js@gmail.com>
2023-08-19 05:01:25 -0300

    feat: add edit task use case

---
+++ src/use-cases/edit-task.spec.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryTasksRepository } from '@/repositories/in-memory/in-memory-tasks-repository'
+
+import { EditTaskUseCase } from './edit-task'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let tasksRepository: InMemoryTasksRepository
+let sut: EditTaskUseCase
+
+describe('Edit a task use case', () => {
+  beforeEach(() => {
+    tasksRepository = new InMemoryTasksRepository()
+    sut = new EditTaskUseCase(tasksRepository)
+  })
+
+  it('should be able to edit a task', async () => {
+    const createdTask = await tasksRepository.create({
+      title: 'to-do 1',
+      userId: 'user-id-01',
+    })
+
+    const { task } = await sut.execute({
+      taskId: createdTask.id,
+      title: 'updated to-do',
+      description: 'updated to-do description',
+      dueDate: new Date(),
+    })
+
+    expect(task.title).toEqual(expect.stringContaining('updated'))
+    expect(task.description).toEqual(expect.stringContaining('updated'))
+    expect(task.dueDate).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to edit a non-existing task', async () => {
+    await expect(() =>
+      sut.execute({
+        taskId: 'non-existing-task-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
---
+++ src/use-cases/edit-task.ts
@@ -0,0 +1,44 @@
+import { TaskDocument } from '@/models/task'
+import { TasksRepository } from '@/repositories/tasks-repository'
+
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface EditTaskUseCaseRequest {
+  taskId: string
+  title?: string
+  description?: string | null
+  dueDate?: Date | null
+}
+
+interface EditTaskUseCaseResponse {
+  task: TaskDocument
+}
+
+export class EditTaskUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async execute({
+    taskId,
+    title,
+    description,
+    dueDate,
+  }: EditTaskUseCaseRequest): Promise<EditTaskUseCaseResponse> {
+    const task = await this.tasksRepository.findById(taskId)
+
+    if (!task) {
+      throw new ResourceNotFoundError()
+    }
+
+    Object.assign(task, {
+      title,
+      description,
+      dueDate,
+    })
+
+    await this.tasksRepository.save(task)
+
+    return {
+      task,
+    }
+  }
+}
