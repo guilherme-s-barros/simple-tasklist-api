c6e35667dc699daada7d37abbb3edb18e1bd649f
Guilherme <gui.barros.js@gmail.com>
2023-08-19 19:50:47 -0300

    feat: add authenticate controller and route

---
+++ src/http/controllers/users/authenticate.ts
@@ -0,0 +1,48 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const useCase = makeAuthenticateUseCase()
+
+    const { user } = await useCase.execute({
+      email,
+      password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.send({
+      token,
+    })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(401).send({
+        message: error.message,
+      })
+    }
+
+    throw error
+  }
+}
--- src/http/routes/users.ts
+++ src/http/routes/users.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from '../controllers/users/authenticate'
 import { register } from '../controllers/users/register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
