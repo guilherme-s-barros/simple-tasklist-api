62597c0a6ef28aca0c5f1b5204f9ec000110d0d4
Guilherme <gui.barros.js@gmail.com>
2023-08-19 03:06:40 -0300

    test: add get task unit test

---
+++ src/use-cases/get-task.spec.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryTasksRepository } from '@/repositories/in-memory/in-memory-tasks-repository'
+
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetTaskUseCase } from './get-task'
+
+let tasksRepository: InMemoryTasksRepository
+
+let sut: GetTaskUseCase
+
+describe('Get a task use case', () => {
+  beforeEach(() => {
+    tasksRepository = new InMemoryTasksRepository()
+
+    sut = new GetTaskUseCase(tasksRepository)
+  })
+
+  it('should be able to get a task', async () => {
+    const createdTask = await tasksRepository.create({
+      title: 'to-do 1',
+      userId: 'user-id-01',
+    })
+
+    const { task } = await sut.execute({
+      taskId: createdTask.id,
+    })
+
+    expect(task.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to get a non-existing task', async () => {
+    await expect(() =>
+      sut.execute({
+        taskId: 'non-existing-task-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
