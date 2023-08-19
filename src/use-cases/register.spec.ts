6f4673fc72f96f8bb46c86524f8e09b93959e5b4
Guilherme <gui.barros.js@gmail.com>
2023-08-18 23:39:59 -0300

    test: create register unit test

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,54 @@
+import { compare } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { RegisterUseCase } from './register'
+
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+describe('Register use case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(usersRepository)
+  })
+
+  it('should be able to register', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash the user password', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordHashed = await compare('123456', user.passwordHash)
+
+    expect(isPasswordHashed).toBe(true)
+  })
+
+  it('should not be able to register with e-mail already taken.', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
