be7692a7dcf066f45dd3a98ed1085d7e4e6e4487
Guilherme <gui.barros.js@gmail.com>
2023-08-19 03:46:27 -0300

    test: add toggle task state unit test

---
+++ src/use-cases/toggle-task-state.spec.ts
@@ -0,0 +1,54 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryTasksRepository } from '@/repositories/in-memory/in-memory-tasks-repository'
+
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { ToggleTaskStateUseCase } from './toggle-task-state'
+
+let tasksRepository: InMemoryTasksRepository
+let sut: ToggleTaskStateUseCase
+
+describe('Toggle task state use case', () => {
+  beforeEach(() => {
+    tasksRepository = new InMemoryTasksRepository()
+    sut = new ToggleTaskStateUseCase(tasksRepository)
+  })
+
+  it('should be able to check a task as completed', async () => {
+    const createdTask = await tasksRepository.create({
+      title: 'to-do 1',
+      userId: 'user-id-01',
+    })
+
+    const { task } = await sut.execute({
+      taskId: createdTask.id,
+    })
+
+    expect(task.completed).toEqual(true)
+  })
+
+  it('should be able to uncheck a completed task', async () => {
+    const createdTask = await tasksRepository.create({
+      title: 'to-do 1',
+      userId: 'user-id-01',
+    })
+
+    await sut.execute({
+      taskId: createdTask.id,
+    })
+
+    const { task } = await sut.execute({
+      taskId: createdTask.id,
+    })
+
+    expect(task.completed).toEqual(false)
+  })
+
+  it('should not be able to check a non-existing task as completed', async () => {
+    await expect(() =>
+      sut.execute({
+        taskId: 'non-existing-task-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
