460a88987b7a2131c9da4096e4989aa6296264ed
Guilherme <gui.barros.js@gmail.com>
2023-08-28 19:58:42 -0300

    refactor: production mail provider example

--- src/queues/processors/factories/make-send-mail-processor.ts
+++ src/queues/processors/factories/make-send-mail-processor.ts
@@ -3,6 +3,8 @@ import { FakeMailProvider } from '@/providers/implementations/fake-mail-provider
 import { SendMailProcessor } from '../send-mail'
 
 export function makeSendMailProcessor() {
+  // const isProduction = env.NODE_ENV === 'production'
+  // const mailProvider = isProduction ? new ExampleProductionMailProvider() : new FakeMailProvider()
   const mailProvider = new FakeMailProvider()
   const processor = new SendMailProcessor(mailProvider)
 
